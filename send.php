da3860cd4ec2aff5f6db61bb5f129895bc0083ed
Greygeler Development <30768789+developer-greygler@users.noreply.github.com>
2021-05-05 17:05:11 +0300

    Update send.php

--- send.php
+++ send.php
@@ -3,7 +3,7 @@
 $comment = stripslashes(htmlspecialchars($_POST['comment']));
 
 // Переменная с текстом
-$text="Поступила заявка:%0A<b>Имя</b>: <i>{$_POST['name']}</i>%0A<b>Телефон</b>: <i>{$_POST['phone']}</i>%0A<b>Комментарий</b>: <i>{$comment}</i>";
+$text=urlencode("Поступила заявка:\n<b>Имя</b>: <i>{$_POST['name']}</i>\n<b>Телефон</b>: <i>{$_POST['phone']}</i>\n<b>Комментарий</b>: <i>{$comment}</i>";
 
  // Код отправки
 file_get_contents("https://api.telegram.org/bot[ТОКЕН БОТА]/sendmessage?chat_id=[ID чата]&parse_mode=html&text={$text}");
